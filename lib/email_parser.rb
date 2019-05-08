29d836ad8153f0d4cbb7a748603ba25dd5d6239a
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-08 20:45:13 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
    
   def parse 
     
-    emails.split(", ").uniq.join(", ")
+    emails.split(", ").uniq
     #.collect do |e_address|     
     #  e_address.split(',').join("")   #takes the value, splits on the ,. use .join to elim. array[]
     #end.uniq          #runs .uniq on the return value last line of code of the line above.
